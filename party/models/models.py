5147761ee737d37fcc8c03db869f3bbc73454a54
Jarenass97 <jorgearenass97@gmail.com>
2021-03-02 22:11:06 +0100

    diseñados temas musicales

--- party/models/models.py
+++ party/models/models.py
@@ -17,6 +17,9 @@ class producer(models.Model):
     name = fields.Char()
     films=fields.Many2many(string="Films",comodel_name="party.film",relation='producers_films',
                            column1='producer_id',column2='film_id')
+    musical_themes = fields.Many2many(string="Musical Themes", comodel_name="party.musical_theme",
+                                      relation='producers_musical_themes',
+                                      column1='producers_id', column2='musical_theme_id')
 
 class screenwriter(models.Model):
     _name = 'party.screenwriter'
@@ -33,6 +36,8 @@ class gender(models.Model):
     name = fields.Char()
     films=fields.Many2many(string="Films",comodel_name="party.film",relation='genders_films',
                            column1='gender_id',column2='film_id')
+    musical_themes=fields.Many2many(string="Musical Themes",comodel_name="party.musical_theme",relation='genders_musical_themes',
+                           column1='gender_id',column2='musical_theme_id')
 
 class film(models.Model):
     _name = 'party.film'
@@ -51,4 +56,23 @@ class film(models.Model):
                                 column1='film_id', column2='gender_id')
     Duration=fields.Integer()
     Language=fields.Selection([('Spanish','Spanish'),('English','English')])
+
+class musical_theme(models.Model):
+    _name = 'party.musical_theme'
+    _description = 'party.musical_theme'
+
+    Title = fields.Char()
+    Album=fields.Char()
+    releaseDate=fields.Date(String="Release date")
+    Format=fields.Char()
+    Genders = fields.Many2many(comodel_name="party.gender", relation='genders_musical_themes',
+                               column1='musical_theme_id', column2='gender_id')
+    Duration=fields.Integer()
+    record_company=fields.Char()
+    Autors=fields.Char()
+    Producers = fields.Many2many(comodel_name="party.producer", relation='producers_musical_themes',
+                                 column1='musical_theme_id', column2='producers_id')
+    Group=fields.Char()
+
+
 
